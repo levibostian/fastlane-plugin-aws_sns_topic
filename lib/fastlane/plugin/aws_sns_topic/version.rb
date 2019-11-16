6c32f30f484a15d4709ba27ee5736642f4606a41
Levi Bostian <levi.bostian@gmail.com>
2019-11-16 13:14:04 -0600

    feat: create action that sends sns topic message
    Note: tests not yet written

---
+++ lib/fastlane/plugin/aws_sns_topic/version.rb
@@ -0,0 +1,5 @@
+module Fastlane
+  module AwsSnsTopic
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/aws_sns_topic_action_spec.rb
@@ -0,0 +1,9 @@
+describe Fastlane::Actions::AwsSnsTopicAction do
+  describe '#run' do
+    it 'prints a message' do
+      # expect(Fastlane::UI).to receive(:message).with("The aws_sns_topic plugin is working!")
+
+      # Fastlane::Actions::AwsSnsTopicAction.run(nil)
+    end
+  end
+end
